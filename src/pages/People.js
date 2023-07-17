d2a34d936a5ff3a8092b5db66fb1dcf3c93d5a50
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-18 01:38:38 +0300

    add loading spinner and update pagination

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,18 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+
 import { moviesContextList } from "../context/moviesList";
-import { Link } from "react-router-dom";
+import Pagination from "../components/Pagination";
 
 import avatar from "../assets/images/Avatar-Profile-Vector-PNG-File.png";
 
 export default function People() {
+
   let {
     trendingPeople,
     searchInput,
+    totalPages,
+    currentPage,
+    isLoading,
+    setCurrentPage,
     handleSearchSubmit,
     handleSearchInput,
-    handlePrevPage,
-    handleNextPage,
   } = useContext(moviesContextList);
+
+  const [searchParams, setSearchParams] = useSearchParams({
+    page: 1,
+  });
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    setSearchParams({ page });
+  };
+
+  useEffect(() => {
+    const pageParam = searchParams.get("page");
+    if (pageParam) {
+      setCurrentPage(Number(pageParam));
+    }
+  }, [searchParams]);
 
   return (
     <>
@@ -31,48 +52,73 @@ export default function People() {
               Search
             </button>
           </form>
-
-          <div className="row justify-content-center align-items-center">
-            {trendingPeople.map((people, i) => {
-              return (
-                <div className="col-6 col-md-3 my-3 mt-5" key={people.id}>
-                  <div className="card">
-                    <Link to={`/people-details/${people.id}`}>
-                      {people.profile_path === null ? (
-                        <img className="w-100" src={avatar} alt="" />
-                      ) : (
-                        <img
-                          className="w-100"
-                          src={`https://image.tmdb.org/t/p/w500${people.profile_path}`}
-                          alt=""
-                        />
-                      )}
-                    </Link>
-                    <div className="card-body" style={{ height: "120px" }}>
-                      <div className="d-flex justify-content-between">
-                        <h5 className="card-title">
-                          <Link to={`/people-details/${people.id}`}>
-                            {" "}
-                            {people.name}{" "}
-                          </Link>
-                        </h5>
+          {isLoading ? (
+            <div
+                className="d-flex justify-content-center align-items-center mt-5"
+              >
+                <i className="fas fa-spinner fa-spin fa-4x"></i>
+              </div>
+          ) : (
+            <>
+              <div className="row justify-content-center align-items-center">
+                {trendingPeople.map((people, i) => {
+                  return (
+                    <div className="col-6 col-md-3 my-3 mt-5" key={people.id}>
+                      <div className="card">
+                        <Link to={`/people-details/${people.id}`}>
+                          {people.profile_path === null ? (
+                            <img className="w-100" src={avatar} alt="" />
+                          ) : (
+                            <img
+                              className="w-100"
+                              src={`https://image.tmdb.org/t/p/w500${people.profile_path}`}
+                              alt=""
+                            />
+                          )}
+                        </Link>
+                        <div className="card-body" style={{ height: "130px" }}>
+                          <div className="d-flex justify-content-between">
+                            <h5 className="card-title">
+                              <Link to={`/people-details/${people.id}`}>
+                                {" "}
+                                {people.name}{" "}
+                              </Link>
+                            </h5>
+                          </div>
+                          <p className="card-text">
+                            <span className="text-primary">Act in:</span>{" "}
+                            {people?.known_for[0]?.title
+                              ? people?.known_for[0]?.title.length > 30
+                                ? `${people?.known_for[0]?.title.slice(
+                                    0,
+                                    30
+                                  )}...`
+                                : people?.known_for[0]?.title
+                              : people?.known_for[0]?.name
+                              ? people?.known_for[0]?.name.length > 30
+                                ? `${people?.known_for[0]?.name.slice(
+                                    0,
+                                    30
+                                  )}...`
+                                : people?.known_for[0]?.name
+                              : ""}
+                          </p>
+                        </div>
                       </div>
-                      <p className="card-text"><span className="text-primary">Act in:</span> {people.known_for[0].title}</p>
                     </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+                  );
+                })}
+              </div>
 
-          <div className="row mb-5 d-flex justify-content-between">
-            <button className="btn col-2 btn-info" onClick={handlePrevPage}>
-              Prev
-            </button>
-            <button className="btn col-2 btn-info" onClick={handleNextPage}>
-              Next
-            </button>
-          </div>
+              {totalPages > 1 && (
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              )}
+            </>
+          )}
         </div>
       </div>
     </>
