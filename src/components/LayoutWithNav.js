f26630f5b2f945125cd2fc818acd8dc80989a419
Safaa-Ebrahim <ebrasafa@gmail.com>
2023-07-19 21:22:04 +0300

    add scroll to top button to the pages

--- src/components/LayoutWithNav.js
+++ src/components/LayoutWithNav.js
@@ -1,6 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
+// components
 import Footer from "./Footer";
 import Header from "./Header";
-import { Outlet } from 'react-router-dom';
+import ScrollToTopBtn from "./ScrollToTopBtn";
 
 
 export default function LayoutWithNav() {
@@ -10,7 +13,8 @@ export default function LayoutWithNav() {
         <div className='container my-5'>
           <Outlet />
         </div>
-        <Footer/>
+        <Footer />
+        <ScrollToTopBtn />
       </>
     )
   }
